0e70cc9118821059390b9fcce1d4af304c76c808
Staffan Småros <staffan.smaros@gmail.com>
2020-10-25 20:33:56 +0200

    Some styling and added books to DOM

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ function addBookToLibrary(library, title, author, pages, isRead) {
     let bookToAdd = new Book(title, author, pages, isRead);
     library.push(bookToAdd);
 }
+
+addBookToLibrary(myLibrary, "Best book", "Staffan", 234, true);
+addBookToLibrary(myLibrary, "Best book 2", "Staffan S", 234, true);
+
+const bookList = document.querySelector(".book-list");
+myLibrary.forEach((book, index) => {
+    let bookItem = document.createElement('li');
+    bookItem.setAttribute("data-index", index);
+    bookItem.textContent = book.info();
+    bookList.appendChild(bookItem);
+})
